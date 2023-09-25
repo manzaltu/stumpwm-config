d3e4ace173d1ea1911648a91349630c0df0fc501
Yoav Orot <orot.yoav@gmail.com>
2023-09-25 16:18:14 -0400

    Enable mode-line on all screens

--- init.lisp
+++ init.lisp
@@ -8,6 +8,10 @@
 (setf *message-window-gravity* :center
       *window-border-style* :thin)
 
+;; Enable mode-line on all screens
+(dolist (head (screen-heads (current-screen)))
+  (enable-mode-line (current-screen) head t))
+
 ;; Optionally, load personal settings
 (let ((personal-file-path (merge-pathnames "personal.lisp" *data-dir*)))
   (when (probe-file personal-file-path)
