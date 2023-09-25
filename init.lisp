fb3cc070ea727f64f4f276db997bd6df50ebf5c9
Yoav Orot <orot.yoav@gmail.com>
2023-09-25 14:34:05 -0400

    Optionally load personal settings from an external file

--- init.lisp
+++ init.lisp
@@ -7,3 +7,8 @@
 ;; General configuration
 (setf *message-window-gravity* :center
       *window-border-style* :thin)
+
+;; Optionally, load personal settings
+(let ((personal-file-path (merge-pathnames "personal.lisp" *data-dir*)))
+  (when (probe-file personal-file-path)
+    (load personal-file-path)))
