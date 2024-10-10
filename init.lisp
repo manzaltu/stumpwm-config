407f52ac637bd2ad07e600db6daaec88197fa66c
Yoav Orot <orot.yoav@gmail.com>
2024-10-10 17:52:05 -0400

    Change gnew, gkill and vgroups command key bindings

--- init.lisp
+++ init.lisp
@@ -35,14 +35,14 @@
 (set-prefix-key (kbd "s-Insert"))
 
 (defvar *group-key-map* (make-sparse-keymap))
-(define-key *group-key-map* (kbd "s-g") "vgroups")
-(define-key *group-key-map* (kbd "s-c") "gnew")
+(define-key *group-key-map* (kbd "s-w") "vgroups")
+(define-key *group-key-map* (kbd "s-g") "gnew")
 (define-key *group-key-map* (kbd "s-o") "gother")
 (define-key *group-key-map* (kbd "s-s") "gselect")
 (define-key *group-key-map* (kbd "s-l") "grouplist")
 (define-key *group-key-map* (kbd "s-m") "gmove")
 (define-key *group-key-map* (kbd "s-M") "gmove-marked")
-(define-key *group-key-map* (kbd "s-k") "gkill")
+(define-key *group-key-map* (kbd "s-c") "gkill")
 (define-key *group-key-map* (kbd "s-r") "grename")
 
 (define-key *top-map* (kbd "s-SPC") "emacs")
